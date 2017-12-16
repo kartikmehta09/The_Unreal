fc1a4f44a8913e6454fe5b1bc5ca428c850f334f
venkateshtata <venkateshtata9@gmail.com>
2017-12-16 07:50:34 +0530

    first commit

---
+++ dcgan.py
@@ -0,0 +1,23 @@
+# Importing the libraries
+from __future__ import print_function
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.optim as optim
+import torch.utils.data
+import torchvision.datasets as dset
+import torchvision.transforms as transforms
+import torchvision.utils as vutils
+from torch.autograd import Variable
+
+
+# Setting some hyperparameters
+batchSize = 64 # We set the size of the batch.
+imageSize = 64 # We set the size of the generated images (64x64).
+
+# Creating the transformations
+transform = transforms.Compose([transforms.Scale(imageSize), transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),]) # We create a list of transformations (scaling, tensor conversion, normalization) to apply to the input images.
+
+# Loading the dataset
+dataset = dset.CIFAR10(root = './data', download = True, transform = transform) # We download the training set in the ./data folder and we apply the previous transformations on each image.
+dataloader = torch.utils.data.DataLoader(dataset, batch_size = batchSize, shuffle = True, num_workers = 2) # We use dataLoader to get the images of the training set batch by batch.
