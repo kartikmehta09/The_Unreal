1c021a1fe6a4815c25f2006837954d16f9ab7f27
venkateshtata <venkateshtata9@gmail.com>
2017-12-17 10:09:38 +0530

    defining the dicriminator

--- dcgan.py
+++ dcgan.py
@@ -60,4 +60,20 @@ class G(nn.Module):
 #creating the generator
 netG = G()
 netG.apply(weights_init)
+
+class D(nn.module):
+    def __init__(self):
+        super(D, self).__init__()
+        self.main = nn.Sequential(
+                nn.Conv2d(3, 64, 4, 2, 1, bias = False),
+                nn.LeakyReLU(0.2, inplace = True),
+                nn.Conv2d(64, 128, 4, 2, 1, bias = False),
+                nn.BatchNorm2d(128),
+                nn.LeakyReLU(0.2, inplace = True),
+                nn.Conv2d(128, 256, 4, 2, 1, bias = False),
+                nn.BatchNorm2d(256),
+                nn.LeakyReLU(0.2, inplace = True),
+                nn.Conv2d(512, 1, 4, 1, 0, bias = False),
+                nn.Sigmoid()
+                )
         
